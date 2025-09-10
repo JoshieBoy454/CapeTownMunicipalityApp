0286d4464b335e709be0bec1be68e1d554c2d9f8
Joshua <Joshuagreggain@gmail.com>
2025-09-10 20:54:35 +0200

    created a database context

---
+++ CapeTownMunicipalityApp/Models/LocalDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CapeTownMunicipalityApp.Models
+{
+    public class LocalDbContext : DbContext
+    {
+        public LocalDbContext(DbContextOptions<LocalDbContext> options) : base(options) { }
+        public DbSet<Report> Report { get; set; }
+        public DbSet<ReportAttatchment> ReportAttatchment { get; set; }
+    }
+}
